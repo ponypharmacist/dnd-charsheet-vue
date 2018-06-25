6d670c74bd75be54dcf41bd97db9e8dc6e53da57
ponypharmacist <evilglen@gmail.com>
2018-06-25 22:42:56 +0300

    removed chalk

--- build/build.js
+++ build/build.js
@@ -5,7 +5,6 @@ process.env.NODE_ENV = 'production'
 var ora = require('ora')
 var rm = require('rimraf')
 var path = require('path')
-var chalk = require('chalk')
 var webpack = require('webpack')
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
@@ -27,14 +26,14 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     }) + '\n\n')
 
     if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
+      console.log('  Build failed with errors.\n')
       process.exit(1)
     }
 
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
+    console.log('  Build complete.\n')
+    console.log(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
-    ))
+    )
   })
 })
