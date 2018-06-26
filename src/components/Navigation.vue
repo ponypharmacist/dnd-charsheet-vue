6068ddc6411c114cb760b11ca35e77d52b2bc70f
ponypharmacist <evilglen@gmail.com>
2018-06-26 08:47:44 +0300

    Fixed navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,28 +4,5 @@
 export default {}
 </script>
 
-<style lang="sass" scoped>
-ul
-    list-style-type: none
-    text-align: center
-    margin: 0
-
-li
-    display: inline-block
-    margin: 0 10px
-
-a
-    color: #fff
-    text-decoration: none
-    padding: 12px
-    border-radius: 5px
-
-nav
-    background: black
-    padding: 30px 0
-    margin-bottom: 40px
-
-.router-link-active
-    background: rgba(255,255,255,0.8)
-    color: #444
+<style>
 </style>
